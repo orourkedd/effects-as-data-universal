db6e630dbc4bf16ddcd5d3a0a80c26ebc77cf0b0
psfrankie <frankie-orourke@pluralsight.com>
2017-07-29 18:45:55 -0700

    fix hoisting naming collision

--- src/handlers/set-immediate.js
+++ src/handlers/set-immediate.js
@@ -1,7 +1,7 @@
 const delay =
   typeof setImmediate === undefined ? fn => setTimeout(fn, 0) : setImmediate
 
-function setImmediate({ cmd }, { call, config, handlers }) {
+function setImmediateHandler({ cmd }, { call, config, handlers }) {
   delay(() => {
     call(config, handlers, function*() {
       yield cmd
@@ -10,5 +10,5 @@ function setImmediate({ cmd }, { call, config, handlers }) {
 }
 
 module.exports = {
-  setImmediate
+  setImmediate: setImmediateHandler
 }
