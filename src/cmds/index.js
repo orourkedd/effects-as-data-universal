897c98af54a4c7e9e053bb9fcb8101f5ecdfd24e
Frankie O'Rourke <frankie@orourkedd.com>
2017-08-04 09:32:41 -0700

    fix setImmediate naming collision in cmd

--- src/cmds/index.js
+++ src/cmds/index.js
@@ -1,19 +1,19 @@
-const { setImmediate } = require('./set-immediate')
-const { call } = require('./call')
-const { echo } = require('./echo')
-const { either } = require('./either')
-const { getState, setState } = require('./state')
-const { guid } = require('./guid')
-const { httpGet, httpPost, httpPut, httpDelete, rpc } = require('./http')
-const { jsonParse } = require('./json-parse')
-const { logInfo, logError } = require('./log')
-const { now } = require('./now')
-const { randomNumber } = require('./random-number')
-const { retry } = require('./retry')
-const { hit } = require('./hit')
+const { setImmediate: setImmediateCmd } = require("./set-immediate")
+const { call } = require("./call")
+const { echo } = require("./echo")
+const { either } = require("./either")
+const { getState, setState } = require("./state")
+const { guid } = require("./guid")
+const { httpGet, httpPost, httpPut, httpDelete, rpc } = require("./http")
+const { jsonParse } = require("./json-parse")
+const { logInfo, logError } = require("./log")
+const { now } = require("./now")
+const { randomNumber } = require("./random-number")
+const { retry } = require("./retry")
+const { hit } = require("./hit")
 
 module.exports = {
-  setImmediate,
+  setImmediate: setImmediateCmd,
   call,
   echo,
   either,
